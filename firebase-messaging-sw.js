48e4986b366e269a402298c91a558cb1b51d7986
pisi107 <giorgio.bruno107@gmail.com>
2026-08-23 21:10:45 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,37 +1,44 @@
-importScripts("https://www.gstatic.com/firebasejs/12.1.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/12.1.0/firebase-messaging-compat.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/12.1.0/firebase-app-compat.js"
+);
 
-firebase.initializeApp({
-    apiKey: "AIzaSyAnu-kly4oALndRaeCl2dvQjWefJSHWZvc",
-    authDomain: "ktm-ordini.firebaseapp.com",
-    projectId: "ktm-ordini",
-    storageBucket: "ktm-ordini.firebasestorage.app",
-    messagingSenderId: "1031736748386",
-    appId: "1:1031736748386:web:b1070dcf67563b84c4643b"
-});
+importScripts(
+  "https://www.gstatic.com/firebasejs/12.1.0/firebase-messaging-compat.js"
+);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAnu-kly4oALndRaeCl2dvQjWefJSHWZvc",
+  authDomain: "ktm-ordini.firebaseapp.com",
+  projectId: "ktm-ordini",
+  storageBucket: "ktm-ordini.firebasestorage.app",
+  messagingSenderId: "1031736748386",
+  appId: "1:1031736748386:web:b1070dcf67563b84c4643b"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
+messaging.onBackgroundMessage(function(payload) {
 
-    console.log(
-        "[firebase-messaging-sw.js] Notifica ricevuta:",
-        payload
-    );
+  console.log(
+    "[firebase-messaging-sw.js] Messaggio ricevuto:",
+    payload
+  );
 
-    const title =
-        payload.notification?.title ||
-        "KTM ORDINI";
+  const notificationTitle =
+    payload.notification?.title || "KTM ORDINI";
 
-    const options = {
-        body:
-            payload.notification?.body ||
-            "Hai ricevuto un nuovo ordine.",
-        icon: "/IDROLIST/favicon.ico"
-    };
+  const notificationOptions = {
+    body:
+      payload.notification?.body ||
+      "È arrivato un nuovo ordine.",
+    icon: "./icon-192.png"
+  };
 
-    self.registration.showNotification(
-        title,
-        options
-    );
+  self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
+
 });
